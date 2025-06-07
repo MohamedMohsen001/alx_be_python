cf96c29926fedbe0718868e74c8a5e9f08e61034
Mohamed Mohsen <marcspecter2091@gmail.com>
2025-06-07 21:32:48 +0300

    added a new file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,4 +7,4 @@ def display_current_datetime():
 def calculate_future_date():
     number_of_days = int(input("Enter Number Of Days:"))
     future_date = datetime.now().date() + timedelta(number_of_days)
-    print(f"{future_date.year}-{future_date.month:02d}-{future_date.day:02d}")
+    print(future_date.strftime("%Y-%m-%d"))
