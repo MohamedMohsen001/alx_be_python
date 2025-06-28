2b547584b49d76efaecaf34c214d2ac8be47d53e
Mohamed Mohsen <marcspecter2091@gmail.com>
2025-06-28 04:17:52 +0300

    added a new file

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,32 @@
+class Book:
+    _is_checked_out = False
+    
+    def __init__(self, title, author):
+         self.title = title
+         self.author = author
+    
+class Library:
+    _books =  []
+    
+    def add_book(self, book):
+        self._books.append(book)
+    
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if not book._is_checked_out == True:
+                    book._is_checked_out = True
+                    self.list_available_books()
+                else:
+                    print(f"'{book.title}' is already checked out.")
+                    return
+                    
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book._is_checked_out = False
+                
+    def list_available_books(self):
+        for book in self._books:
+            if book._is_checked_out == False:
+                print(f"{book.title} by {book.author}")
